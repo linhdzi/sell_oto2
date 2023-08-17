a8c3a11d1c0a6cdc48e3a46caffde29446afdca3
pecklo <135631692+KhanhVuQuoc@users.noreply.github.com>
2023-08-17 15:50:31 +0700

    Update CustomerController.php

--- e_project/app/Http/Controllers/CustomerController.php
+++ e_project/app/Http/Controllers/CustomerController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 use App\Models\categories;
 use Illuminate\Http\Request;
 
@@ -25,13 +27,164 @@ class CustomerController extends Controller
     }
     public function shop($id){
         $products = DB::table('products')
-                    ->select('*')
+                    ->select('products.id','products.pic','products.name','products.description','products.price',)
                     ->join('category_products', 'products.id', '=', 'category_products.products_id')
                     ->join('categories','categories.id','=','category_products.categories_id')
                     ->where('category_products.categories_id','=',$id)
                     ->get();
+        
         return view('customers.shop',compact('products'));
     }
+    public function getAll(Request $request){
+        $keyword = "";
+        if($request != null){
+            $keyword = $request -> input("_keyword");
+        }
+        $results = null;
+        if($keyword == ""){
+            //Lấy ra toàn bo
+            $results = DB::table('products')
+                      ->select('*')
+                      ->get();
+        }
+        if($keyword != ""){
+            //Lay ra config theo tim kiem
+            $results = DB::table('products')
+                            ->where('name','like','%'.$keyword.'%')
+                            ->get();
+        }
+        return view('customers.search',compact('keyword','results'));
+    }
+    public function createAccount(Request $request){
+        $username="";
+        $password="";
+        $confirm_password = "";
+        $results= null;
+        if($request != null){
+            $username = $request -> input("username");
+            $password = $request -> input("password");
+            $confirm_password = $request -> input("confirm_password");
+        }
+        if($password == $confirm_password){
+            $value = DB::table('customers')->insert([
+                'phone_number' => $username,
+                'password' => $password,
+            ]);    
+            echo"<script>alert('Dang ky thanh cong');</script>";
+        }else{
+            echo"<script>alert('Dang ky that bai');</script>";
+        }
+        return view('customers.login');
+    }
+    public function Access(Request $request)
+    {
+        $results = $this->getCate();
 
-  
+        if ($request != null && $request->has('username') && $request->has('password')) {
+            $username = $request->input('username');
+            $password = $request->input('password');
+            
+            $value = DB::table('customers')
+                ->select('*')
+                ->where('phone_number', '=', $username)
+                ->where('password', '=', $password)
+                ->get();
+                $user = $value->first();
+            if ($value->count() > 0) {
+                echo "<script>alert('Đăng nhập thành công');</script>";
+
+                Session::put('id_customer',$user -> id);
+                Session::put('username',$username);
+                Session::put('password',$password);
+                
+                return $results;
+            } else {
+                echo "<script>alert('Đăng nhập thất bại');</script>";
+                return view('customers.login');
+            }
+        } else {
+            echo "<script>alert('Dữ liệu không hợp lệ');</script>";
+            return view('customers.login');
+        }
+    }
+    public function Logout(){
+        Session::flush();
+        return view('customers.login');
+    }
+
+    public function Addcart($id)
+    {
+        $customerId = Session::get('id_customer');
+        if ($customerId) {
+            $value = DB::table('customers_products')    
+                ->select('*')
+                ->where('customers_id', '=', $customerId)
+                ->where('products_id', '=', $id)
+                ->first();
+    
+            if ($value) {
+                $quantity = $value->quantity + 1;
+                DB::table('customers_products')
+                    ->where('customers_id', $customerId)
+                    ->update(['quantity' => $quantity]);
+            } else {
+                DB::table('customers_products')->insert([
+                    'customers_id' => $customerId,
+                    'products_id' => $id,
+                    'quantity' => 1,
+                ]);
+            }
+            echo "<script>alert('Add cart thành công');</script>";
+            return redirect()->back();
+        } else {
+            echo "<script>alert('Add cart fail pls login');</script>";
+            return redirect()->back();
+        }
+    }
+    
+    public function Rendercard(){
+            $number = 0;
+            $id = Session::get('id_customer');
+            if($id){
+                $results = DB::table('products')
+                       ->select('*')
+                       ->join('customers_products', 'products.id', '=', 'customers_products.products_id')
+                       ->where('customers_products.customers_id','=', $id)
+                       ->get();
+                foreach($results as $result){
+                    $result -> totalprice = $result -> quantity * $result -> price;
+                }
+                return view('customers.cart',compact('results','number')) ;
+            }else{
+                echo "<script>alert('Pls Login');</script>";
+                return view('customers.login');
+            }
+
+                      
+        }
+    public function Deletecard($id){
+            $id_customer = Session::get('id_customer');
+            $results = DB::table('customers_products')
+                       ->select('*')
+                       ->where('customers_products.customers_id','=', $id_customer)
+                       ->where('customers_products.products_id','=', $id)
+                       ->first();
+            $value=$results -> quantity;           
+            if($value >1){
+                DB::table('customers_products')
+                
+                ->where('customers_products.customers_id','=', $id_customer)
+                ->where('customers_products.products_id','=', $id)
+                ->update(['quantity' => $results -> quantity -1 ]);
+            }else{
+                DB::table('customers_products')           
+                ->where('customers_products.customers_id','=', $id_customer)
+                ->where('customers_products.products_id','=', $id)
+                ->delete();
+                ;
+            }
+            $return = $this->Rendercard();
+            return $return;
+    }
+    
 }
